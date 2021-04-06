eb04d6f3010a7f1403c9a9b2f419b67b05da3d14
soniceelijah <soniceelijah@yandex.ru>
2021-04-06 11:22:07 +0000

    style minor

--- services/chat.service.js
+++ services/chat.service.js
@@ -4,6 +4,8 @@ const $ = require('../mappers/_index.map');
 /* buisness logic functions */
 function testone() { return "hello from chat service"; }
 
+// do smth
+
 moddule.exports = { 
     testone,
 };
